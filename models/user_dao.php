85a9134dfc43008828889ddef67c78d0b5c68d65
dennisjian <taiwinner0192@gmail.com>
2020-06-17 16:07:50 +0800

    新增：操作紀錄功能 修改：智能客服基本設定邏輯改寫、各參數敘述

--- models/user_dao.php
+++ models/user_dao.php
@@ -8,36 +8,77 @@ class user_dao
 {
     private static $table_name = "bg_user";
 
+    /**
+     * 抓取使用者資料
+     * @return array 回傳table表單資料
+     */
     function getUser()
     {
         return DB::select("SELECT `id`,`user_name`,`authority`,`account`,`create_dt` FROM " . user_dao::$table_name);
     }
 
+    /**
+     * 搜尋要查詢的使用者資料
+     * @param mixed $act 使用者帳號
+     * @return array 回傳table表單資料
+     */
     function selectUser($act)
     {
         return DB::select("SELECT * FROM " . user_dao::$table_name . " WHERE account = '" . $act . "'");
     }
 
+    /**
+     * 寫入使用者資料
+     * @param mixed $act 使用者帳號
+     * @param mixed $pad 使用者密碼
+     * @param mixed $name 使用者名稱
+     * @param mixed $aut 使用者權限
+     * @param mixed $time 建立時間
+     * @return bool 回傳是否成功
+     */
     function insertUser($act, $pad, $name, $aut, $time)
     {
         return DB::DBCode("INSERT INTO " . user_dao::$table_name . " (account,password,user_name,authority,create_dt) VALUES ('" . $act . "','" . $pad . "','" . $name . "','" . $aut . "','" . date("Y-m-d H:i:s", $time) . "')");
     }
 
+    /**
+     * 更新使用者資料
+     * @param mixed $id 使用者編號
+     * @param mixed $name 使用者名稱
+     * @param mixed $aut 使用者權限
+     * @return bool 回傳是否成功
+     */
     function updateUserForEdit($id, $name, $aut)
     {
         return DB::DBCode("UPDATE " . user_dao::$table_name . " SET user_name = '" . $name . "',authority = '" . $aut . "' WHERE id = '" . $id . "'");
     }
 
+    /**
+     * 抓取指定使用者的資料
+     * @param mixed $id
+     * @return array 回傳table表單資料
+     */
     function selectUserByID($id)
     {
         return DB::select("SELECT * FROM " . user_dao::$table_name . " WHERE id = '" . $id . "'");
     }
 
+    /**
+     * 更新使用者密碼
+     * @param mixed $id 使用者編號
+     * @param mixed $pad 使用者密碼
+     * @return bool 回傳是否成功
+     */
     function updateUserForPad($id, $pad)
     {
         return DB::DBCode("UPDATE " . user_dao::$table_name . " SET password ='" . $pad . "' WHERE id ='" . $id . "'");
     }
 
+    /**
+     * 刪除使用者資料
+     * @param mixed $id 使用者編號
+     * @return bool 回傳是否成功
+     */
     function setDelete($id)
     {
         return DB::DBCode("UPDATE `" . user_dao::$table_name . "` SET `is_del` = 1 WHERE `id` = '" . $id . "';");
--- models/whitelist_dao.php
+++ models/whitelist_dao.php
@@ -8,22 +8,44 @@ class whitelist_dao
 {
     private static $table = "whitelist";
 
+    /**
+     * 抓取IP白名單列表
+     * @param mixed $page 當前頁碼
+     * @param mixed $limit 要抓取的筆數
+     * @return array 回傳table表單資料
+     */
     function getList($page, $limit)
     {
         return DB::select("SELECT * FROM `" . whitelist_dao::$table . "` WHERE `is_del` = 0 LIMIT " . ($page - 1) * $limit . "," . $limit . ";");
     }
 
+    /**
+     * 抓取指定IP的資料
+     * @param mixed $ip 指定的IP
+     * @return array 回傳table表單資料
+     */
     function getIP($ip)
     {
         return DB::select("SELECT * FROM `" . whitelist_dao::$table . "` WHERE `ip` = '" . $ip . "' AND `is_del` = 0 LIMIT 1");
     }
 
+    /**
+     * 新增IP
+     * @param mixed $ip 要新增的IP
+     * @return bool 回傳是否成功
+     */
     function insertIP($ip)
     {
         return DB::DBCode("INSERT INTO `" . whitelist_dao::$table . "` (`ip`,`creator`,`creator_name`,`creation_date`,`updater`,`updater_name`,`update_date`) 
                            VALUE ('" . $ip . "','" . $_SESSION["act"] . "','" . $_SESSION["name"] . "','" . date("Y-m-d H:i:s") . "','" . $_SESSION["act"] . "','" . $_SESSION["name"] . "','" . date("Y-m-d H:i:s") . "')");
     }
 
+    /**
+     * 更新指定的IP
+     * @param mixed $id 指定的IP編號
+     * @param mixed $ip 更新的IP
+     * @return bool 回傳是否成功
+     */
     function setIP($id, $ip)
     {
         return DB::DBCode("UPDATE `" . whitelist_dao::$table . "` 
@@ -31,6 +53,11 @@ class whitelist_dao
                            WHERE `id` = '" . $id . "'");
     }
 
+    /**
+     * 刪除指定的IP
+     * @param mixed $id 指定的IP編號
+     * @return bool 回傳是否成功
+     */
     function deleteIP($id)
     {
         return DB::DBCode("UPDATE `" . whitelist_dao::$table . "` 
